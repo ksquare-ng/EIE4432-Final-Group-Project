969c2501b8c2b45611cf462d45df3d29a5ecd3cf
aidinatt8 <69782691+aidinatt8@users.noreply.github.com>
2020-12-15 15:49:26 +0600

    Create sidebar.php

---
+++ sidebar.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+    <title> Home </title>
+    <link rel = "stylesheet" href = "sidebar.css">
+    <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+</head>
+<body>
+<input type = "checkbox" id = "check">
+<label for = "check">
+    <i class = "fas fa-bars" id = "btn"></i>
+    <i class = "fas fa-times" id = "cancel"></i>
+</label>
+    <div class = "sidebar">
+        <header> Sidebar </header>
+        <ul>
+            <li><a href = "#"><i class = "fas fa-qrcode"></i>Dashboard</a></li>
+            <li><a href = "#"><i class = "fas fa-stream"></i>Exams</a></li>
+            <li><a href = "#"><i class = "fas fa-chalkboard-teacher"></i>Grades</a></li>
+            <li><a href = "#"><i class = "fas fa-power-off"></i>Logout</a></li>
+        </ul>
+    </div>
+    
+    <section> 
+        <div class = "header">Welcome! Have a nice day!</div>
+        <div class = "info"> 
+            <div>ddbf</div>
+        </div>
+    </section>
+
+</body>
+</html>
